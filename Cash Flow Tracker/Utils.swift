d4ea884682cea7aa0dc85fec7f1c1a0c387704d0
Joe <joebuchoff@gmail.com>
2020-09-25 12:09:51 -0500

    Added BudgetType enum and Budget class.

--- Cash Flow Tracker/Utils.swift
+++ Cash Flow Tracker/Utils.swift
@@ -8,6 +8,39 @@
 
 import Foundation
 
+enum BudgetType
+{
+    case Food
+    case Gas
+    case Gym
+    case Rent
+    case Utilities
+    case Subscriptions
+    case Toiletries_and_Cleaning_Supplies
+    case Car_Payments
+    case Car_Insurance
+}
+
+class Budget
+{
+    /* Properties */
+    // $$$ amount in this Budget
+    var amount: Float
+    
+    // Is this an expense or income?
+    var type: BudgetType
+    
+    // Full initializer
+    init (_ amount: Float, type: BudgetType)
+    {
+        // Set self.amount to passed value
+        self.amount = amount
+        
+        // Set whether this is an expense or income
+        self.type = type
+    }
+}
+
 enum EntryType
 {
     case Income
@@ -20,10 +53,10 @@ class Entry
     // $$$ amount in this Entry
     var amount: Float
     // String holding dollar sign in front of the amount with 2 decimal places, if it's an expense, put a negative sign in front of it and negate the negative in the amount
-    var amountString: String { return (entryType == .Expense) ? "-$\(String(format: "%.2f", -amount))" : "$\(String(format: "%.2f", amount))" }
+    var amountString: String { return (type == .Expense) ? "-$\(String(format: "%.2f", -amount))" : "$\(String(format: "%.2f", amount))" }
     
     // Is this an expense or income?
-    var entryType: EntryType
+    var type: EntryType
     
     // Full initializer
     init (_ amount: Float)
@@ -34,9 +67,9 @@ class Entry
         // Set whether this is an expense or income
         if self.amount < 0
         {
-            self.entryType = .Expense
+            self.type = .Expense
         } else {
-            self.entryType = .Income
+            self.type = .Income
         }
     }
     
